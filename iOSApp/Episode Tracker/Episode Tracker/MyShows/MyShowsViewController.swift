1e517b1653f006e2fc7c81d3b30ad0c0db27fbc6
Yury Polek <yury.polek@gmail.com>
2019-12-12 01:18:51 +0200

    add Table View to My Shows ViewController

---
+++ iOSApp/Episode Tracker/Episode Tracker/MyShows/MyShowsViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class MyShowsViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension MyShowsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
