117cff7ceabf83a59946edee1a02fe4c11ef0e6c
Yury Polek <yury.polek@gmail.com>
2019-12-12 12:24:39 +0200

    recreate iOS app and setup CocoaPods

--- iOSApp/EpisodeTracker/EpisodeTracker/AppDelegate.swift
+++ iOSApp/EpisodeTracker/EpisodeTracker/AppDelegate.swift
@@ -1,3 +1,11 @@
+//
+//  AppDelegate.swift
+//  EpisodeTracker
+//
+//  Created by Yury Polek on 12.12.2019.
+//  Copyright © 2019 Yury Polek. All rights reserved.
+//
+
 import UIKit
 
 @UIApplicationMain
@@ -6,6 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
         return true
     }
+
+
 }
+
