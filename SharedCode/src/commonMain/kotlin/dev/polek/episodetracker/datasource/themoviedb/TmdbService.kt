9cc4a4b6de9e435d32b8ea67a8be0478dd6d2421
Yury Polek <yury.polek@gmail.com>
2019-12-15 20:50:32 +0200

    add JsonSerializer to Ktor client

--- SharedCode/src/commonMain/kotlin/dev/polek/episodetracker/datasource/themoviedb/TmdbService.kt
+++ SharedCode/src/commonMain/kotlin/dev/polek/episodetracker/datasource/themoviedb/TmdbService.kt
@@ -2,11 +2,15 @@ package dev.polek.episodetracker.datasource.themoviedb
 
 import dev.polek.episodetracker.THE_MOVIE_DB_API_ACCESS_TOKEN
 import dev.polek.episodetracker.common.network.bearer
+import dev.polek.episodetracker.datasource.themoviedb.entities.SearchResultPageEntity
 import io.ktor.client.HttpClient
 import io.ktor.client.features.auth.Auth
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.features.logging.LogLevel
 import io.ktor.client.features.logging.Logging
 import io.ktor.client.request.get
+import kotlinx.serialization.json.Json
 
 class TmdbService {
 
@@ -19,12 +23,15 @@ class TmdbService {
         install(Logging) {
             level = LogLevel.ALL
         }
+        install(JsonFeature) {
+            serializer = KotlinxSerializer(json = Json.nonstrict)
+        }
     }
 
-    suspend fun search(query: String, page: Int = 1): String {
+    suspend fun search(query: String, page: Int = 1): SearchResultPageEntity {
         require(page >= 1) { "Page number must be >= 1" }
 
-        return client.get<String>("$BASE_URL/search/tv?query=$query&page=$page")
+        return client.get("$BASE_URL/search/tv?query=$query&page=$page")
     }
 
     companion object {
