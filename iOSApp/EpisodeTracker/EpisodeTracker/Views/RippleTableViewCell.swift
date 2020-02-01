d22aecda5d1d67dd6c8b61ef57f8ebef775ed954
Yury Polek <yury.polek@gmail.com>
2020-02-01 12:57:30 +0200

    call super implementation of touchBegan/Ended/Canceled to fix table cell selection

--- iOSApp/EpisodeTracker/EpisodeTracker/Views/RippleTableViewCell.swift
+++ iOSApp/EpisodeTracker/EpisodeTracker/Views/RippleTableViewCell.swift
@@ -6,16 +6,19 @@ class RippleTableViewCell: UITableViewCell {
     @IBOutlet weak var rippleView: MDCRippleView!
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
         if let location = touches.first?.location(in: rippleView) {
             rippleView.beginRippleTouchDown(at: location, animated: true, completion: nil)
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
         rippleView.beginRippleTouchUp(animated: true, completion: nil)
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
         rippleView.beginRippleTouchUp(animated: true, completion: nil)
     }
 }
