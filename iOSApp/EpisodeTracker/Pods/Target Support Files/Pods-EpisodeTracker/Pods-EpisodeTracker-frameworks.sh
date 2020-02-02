474e52d5d8f4572f52eee734645023f7b91ba00c
Yury Polek <yury.polek@gmail.com>
2020-02-02 12:10:57 +0200

    add MaterialComponents/Tabs pod

--- iOSApp/EpisodeTracker/Pods/Target Support Files/Pods-EpisodeTracker/Pods-EpisodeTracker-frameworks.sh
+++ iOSApp/EpisodeTracker/Pods/Target Support Files/Pods-EpisodeTracker/Pods-EpisodeTracker-frameworks.sh
@@ -162,10 +162,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MDFInternationalization/MDFInternationalization.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MaterialComponents/MaterialComponents.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MDFInternationalization/MDFInternationalization.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MaterialComponents/MaterialComponents.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
