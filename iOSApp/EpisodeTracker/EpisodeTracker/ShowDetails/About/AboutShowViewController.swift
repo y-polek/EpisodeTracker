43e562ff91d87da6b2b69aefa348fa9346957d16
Yury Polek <yury.polek@gmail.com>
2020-02-14 00:06:44 +0200

    show genres in AboutShowViewController

--- iOSApp/EpisodeTracker/EpisodeTracker/ShowDetails/About/AboutShowViewController.swift
+++ iOSApp/EpisodeTracker/EpisodeTracker/ShowDetails/About/AboutShowViewController.swift
@@ -3,9 +3,39 @@ import SharedCode
 
 class AboutShowViewController: UIViewController {
     
+    @IBOutlet weak var genresCollectionView: UICollectionView!
+    
     var showId: Int!
+    var presenter: AboutShowPresenter!
+    var genres = [String]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        presenter = AboutShowPresenter(showId: Int32(showId), repository: AppDelegate.instance().myShowsRepository)
+        presenter.attachView(view: self)
+    }
+}
+
+// MARK: - AboutShowView implementation
+extension AboutShowViewController: AboutShowView {
+    func displayShowDetails(show: ShowDetailsViewModel) {
+        genres = show.genres
+        genresCollectionView.reloadData()
+    }
+}
+
+// MARK: - UICollectionView datasource and delegate
+extension AboutShowViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return genres.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "genre_cell", for: indexPath) as! GenreCell
+        let genre = genres[indexPath.row]
+        cell.label.text = genre
+        return cell
     }
 }
---
+++ iOSApp/EpisodeTracker/EpisodeTracker/ShowDetails/About/GenreCell.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class GenreCell: UICollectionViewCell {
+    
+    @IBOutlet weak var shadowView: UIView!
+    @IBOutlet weak var label: UILabel!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setup()
+    }
+    
+    private func setup() {
+        shadowView.backgroundColor = .white
+        shadowView.layer.cornerRadius = 16
+        
+        shadowView.layer.shadowColor = UIColor.lightGray.cgColor
+        shadowView.layer.shadowOffset = CGSize(width: 0, height: 1)
+        shadowView.layer.shadowOpacity = 0.7
+        shadowView.clipsToBounds = false
+    }
+}
