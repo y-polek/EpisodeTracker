1693cc6900e2e25b6059658748b158375fa8084d
Yury Polek <yury.polek@gmail.com>
2020-02-22 22:37:45 +0200

    add private access modifier to internal fields

--- SharedCode/src/commonMain/kotlin/dev/polek/episodetracker/common/datasource/themoviedb/entities/ShowDetailsEntity.kt
+++ SharedCode/src/commonMain/kotlin/dev/polek/episodetracker/common/datasource/themoviedb/entities/ShowDetailsEntity.kt
@@ -11,22 +11,22 @@ import kotlinx.serialization.Transient
 data class ShowDetailsEntity(
     @SerialName("id") val tmdbId: Int? = null,
     @SerialName("name") val name: String? = null,
-    @SerialName("first_air_date") val firstAirDate: String? = null,
-    @SerialName("last_air_date") val lastAirDate: String? = null,
-    @SerialName("genres") val genreEntities: List<GenreEntity>? = null,
-    @SerialName("networks") val networks: List<NetworkEntity>? = null,
+    @SerialName("first_air_date")private val firstAirDate: String? = null,
+    @SerialName("last_air_date") private val lastAirDate: String? = null,
+    @SerialName("genres") private val genreEntities: List<GenreEntity>? = null,
+    @SerialName("networks") private val networks: List<NetworkEntity>? = null,
     @SerialName("overview") val overview: String? = null,
     @SerialName("poster_path") val posterPath: String? = null,
     @SerialName("backdrop_path") val backdropPath: String? = null,
-    @SerialName("homepage") val homepage: String? = null,
+    @SerialName("homepage") private val homepage: String? = null,
     @SerialName("in_production") val inProduction: Boolean = true,
     @SerialName("next_episode_to_air") val nextEpisodeToAir: EpisodeEntity? = null,
     @SerialName("number_of_seasons") val numberOfSeasons: Int = 1,
     @SerialName("external_ids") val externalIds: ExternalIdsEntity? = null,
-    @SerialName("content_ratings") val contentRatings: ContentRatingsEntity? = null,
-    @SerialName("videos") val videosEntity: VideosEntity? = null,
-    @SerialName("credits") val credits: CreditsEntity? = null,
-    @SerialName("recommendations") val recommendationsEntity: RecommendationsEntity? = null)
+    @SerialName("content_ratings") private val contentRatings: ContentRatingsEntity? = null,
+    @SerialName("videos") private val videosEntity: VideosEntity? = null,
+    @SerialName("credits") private val credits: CreditsEntity? = null,
+    @SerialName("recommendations") private val recommendationsEntity: RecommendationsEntity? = null)
 {
     @Transient val isValid = allNotNull(tmdbId, name, numberOfSeasons)
     @Transient val year: Int? = firstAirDate?.let(::parseDate)?.year
