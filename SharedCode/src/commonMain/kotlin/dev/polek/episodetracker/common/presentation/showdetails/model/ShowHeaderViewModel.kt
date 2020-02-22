e78ffb06b34ac07d38e7e93c4af40fec74df228d
Yury Polek <yury.polek@gmail.com>
2020-02-22 17:11:52 +0200

    move Show subheader creation to ViewModel

--- SharedCode/src/commonMain/kotlin/dev/polek/episodetracker/common/presentation/showdetails/model/ShowHeaderViewModel.kt
+++ SharedCode/src/commonMain/kotlin/dev/polek/episodetracker/common/presentation/showdetails/model/ShowHeaderViewModel.kt
@@ -1,8 +1,25 @@
 package dev.polek.episodetracker.common.presentation.showdetails.model
 
-data class ShowHeaderViewModel(
+class ShowHeaderViewModel(
     val name: String,
     val imageUrl: String?,
-    val subhead: String,
-    val rating: String
-)
+    val rating: String,
+    year: Int?,
+    endYear: Int?,
+    network: String?)
+{
+    val subhead: String
+
+    init {
+        val yearsText = when {
+            year != null && endYear != null -> "$year - $endYear"
+            year != null -> "$year"
+            else -> null
+        }
+        subhead = when {
+            yearsText != null && network != null -> "$yearsText ∙ $network"
+            yearsText != null -> "$yearsText - $endYear"
+            else -> ""
+        }
+    }
+}
--- SharedCode/src/commonMain/kotlin/dev/polek/episodetracker/common/presentation/showdetails/presenter/ShowDetailsPresenter.kt
+++ SharedCode/src/commonMain/kotlin/dev/polek/episodetracker/common/presentation/showdetails/presenter/ShowDetailsPresenter.kt
@@ -24,26 +24,13 @@ class ShowDetailsPresenter(
             return
         }
 
-        val year = show.year
-        val endYear = if (show.isEnded) show.lastYear else null
-        val yearsText = when {
-            year != null && endYear != null -> "$year - $endYear"
-            year != null -> "$year"
-            else -> null
-        }
-        val network = show.networkName
-
-        val subhead = when {
-            yearsText != null && network != null -> "$yearsText ∙ $network"
-            yearsText != null -> "$yearsText - $endYear"
-            else -> ""
-        }
-
         val headerViewModel = ShowHeaderViewModel(
             name = show.name,
             imageUrl = show.imageUrl,
-            subhead = subhead,
-            rating = show.contentRating.orEmpty())
+            rating = show.contentRating.orEmpty(),
+            year = show.year,
+            endYear = if (show.isEnded) show.lastYear else null,
+            network = show.networkName)
 
         view?.displayShowHeader(headerViewModel)
     }
