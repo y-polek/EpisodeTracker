f7cc5d9db08016b548c5bd6f4d4263742ccf7b7f
Yury Polek <yury.polek@gmail.com>
2020-03-15 16:35:36 +0200

    print timestamp and tag in log message

--- SharedCode/src/iosMain/kotlin/dev/polek/episodetracker/common/network/HttpClients.kt
+++ SharedCode/src/iosMain/kotlin/dev/polek/episodetracker/common/network/HttpClients.kt
@@ -1,9 +1,11 @@
 package dev.polek.episodetracker.common.network
 
+import dev.polek.episodetracker.common.logging.log
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.ios.Ios
 import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
 import io.ktor.client.features.logging.Logging
 import platform.Foundation.NSURLRequestUseProtocolCachePolicy
 
@@ -18,5 +20,10 @@ actual fun platformHttpClient(setup: HttpClientConfig<*>.() -> Unit) = HttpClien
 
     install(Logging) {
         level = LogLevel.ALL
+        logger = object : Logger {
+            override fun log(message: String) {
+                log("Ktor") { message }
+            }
+        }
     }
 }
