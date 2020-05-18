f03b6b5d1f976c4af513768e099ff73800fb3981
Yury Polek <yury.polek@gmail.com>
2020-05-18 20:37:38 +0300

    remove unnecessary code from DiscoverFragment layout

--- AndroidApp/src/main/java/dev/polek/episodetracker/discover/DiscoverFragment.kt
+++ AndroidApp/src/main/java/dev/polek/episodetracker/discover/DiscoverFragment.kt
@@ -5,20 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
-import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.appbar.AppBarLayout
 import dev.polek.episodetracker.App
-import dev.polek.episodetracker.R
 import dev.polek.episodetracker.common.presentation.discover.DiscoverView
-import dev.polek.episodetracker.databinding.DiscoverFragmentBinding
 import dev.polek.episodetracker.common.presentation.discover.model.DiscoverResultViewModel
+import dev.polek.episodetracker.databinding.DiscoverFragmentBinding
 import dev.polek.episodetracker.utils.HideKeyboardScrollListener
-import dev.polek.episodetracker.utils.scrollFlags
-import dev.polek.episodetracker.utils.setTopMargin
-import dev.polek.episodetracker.utils.setTopPadding
 
 class DiscoverFragment : Fragment(), DiscoverView {
 
@@ -42,41 +36,18 @@ class DiscoverFragment : Fragment(), DiscoverView {
         }
         binding.recyclerView.adapter = adapter
 
-        binding.appBarLayout.outlineProvider = null
-
         binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextChange(query: String): Boolean {
-                setSearchBarBehaviour(showAlways = query.isNotBlank())
-                return true
-            }
 
             override fun onQueryTextSubmit(query: String): Boolean {
                 binding.searchView.clearFocus()
                 presenter.onSearchQuerySubmitted(query.trim())
                 return true
             }
-        })
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, insets ->
-            val searchBarMarginTop = resources.getDimensionPixelOffset(R.dimen.search_bar_margin_top)
-            val searchBarMarginBottom = resources.getDimensionPixelOffset(R.dimen.search_bar_margin_bottom)
-            binding.searchBar.setTopMargin(searchBarMarginTop + insets.systemWindowInsetTop)
-
-            val recyclerViewTopPadding =
-                searchBarMarginTop + searchBarMarginBottom + binding.searchBar.height + insets.systemWindowInsetTop
-            if (binding.recyclerView.paddingTop != recyclerViewTopPadding) {
-                binding.recyclerView.setTopPadding(recyclerViewTopPadding)
-                binding.recyclerView.scrollToPosition(0)
+            override fun onQueryTextChange(query: String): Boolean {
+                return true
             }
-
-            return@setOnApplyWindowInsetsListener insets
-        }
-
-
-
-        val results = listOf<DiscoverResultViewModel>()
-        adapter.results = results
-
+        })
 
         return binding.root
     }
@@ -151,18 +122,6 @@ class DiscoverFragment : Fragment(), DiscoverView {
 
     override fun openDiscoverShow(show: DiscoverResultViewModel) {
 
-    }
-
-    private fun setSearchBarBehaviour(showAlways: Boolean) {
-        binding.searchBar.scrollFlags = if (showAlways) 0 else AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL or AppBarLayout.LayoutParams.SCROLL_FLAG_ENTER_ALWAYS
-    }
-
-    private fun makeSearchBarVisible() {
-        val scrollFlags = binding.searchBar.scrollFlags
-        binding.searchBar.scrollFlags = 0
-        binding.root.post {
-            binding.searchBar.scrollFlags = scrollFlags
-        }
     }
 
     companion object {
