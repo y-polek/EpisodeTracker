588481f7ed0057d60f8a1cd4c993430c89ab8911
Yury Polek <yury.polek@gmail.com>
2020-06-14 20:10:18 +0300

    change SharedCode module package name
    fixes "BuildConfig is defined multiple times" error

--- SharedCode/src/androidMain/kotlin/dev/polek/episodetracker/common/utils/Build.kt
+++ SharedCode/src/androidMain/kotlin/dev/polek/episodetracker/common/utils/Build.kt
@@ -1,6 +1,6 @@
 @file:JvmName("BuildActual")
 package dev.polek.episodetracker.common.utils
 
-import dev.polek.episodetracker.BuildConfig
+import dev.polek.episodetracker.common.BuildConfig
 
 actual val DEBUG_BUILD: Boolean = BuildConfig.DEBUG
